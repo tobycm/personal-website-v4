3f1b85c18f1b5416674cd3345466bb74addf6b85
Toby Cm <toby@vsus.app>
2024-03-08 17:02:23 -0800

    why test for a frontend project :b
    Signed-off-by: Toby Cm <toby@vsus.app>

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,20 +1,15 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './vitest.setup.mjs',
-  },
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
           }
         },
       },
